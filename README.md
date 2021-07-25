85e599d7e23933bc1fc372a475e4bfbfe7e7900e
Furkan Alp Tokaç <furkanalptokac@gmail.com>
2021-07-25 18:12:19 +0300

    added feature: daily increase or decrease in cases, deaths.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # twitter-api-covid
+
+A bot that tweets daily Covid-19 cases and deaths of Turkey everyday at 21:00 by default. Data from https://covid19.saglik.gov.tr/
+
+**!!** Before using, update yesterday's case and death info in the ```yesterday.json``` file.
+
+Sample tweet:
+
+![](public/img/tweet.png)
+
+**Note:** I can't fix the ```Error: Uncaught [ReferenceError: $ is not defined]``` error. But it works.
+
+No licence, feel free to use and improve.
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const Twitter = require('twitter-lite');
-const axios = require("axios").default;
+const axios = require("axios");
+const cron = require('node-cron');
+const { JSDOM } = require('jsdom');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('./yesterday.json');
+const db = low(adapter);
+
+db.defaults({ yesterday: {} })
+  .write();
 
 require('dotenv').config();
 
@@ -12,31 +22,59 @@ const client = new Twitter({
 
 const options = {
   method: 'GET',
-  url: 'https://coronavirus-19-api.herokuapp.com/countries'
+  url: 'https://covid19.saglik.gov.tr/'
 };
 
-axios.request(options).then((response) => {
-  const data = response.data;
-  // If you want to see all of the COVID-19 situation around the world, remove the comment.
-  // console.table(data);
+cron.schedule('00 21 * * *', () => {
+  try {
+    axios.request(options).then(response => {
+      const html = response.data;
 
-  data.forEach(key => {
-    // You can also change the country value whatever you want.
-    if (key.country === 'Turkey') {
+      const window = new JSDOM(html, {
+        runScripts: "dangerously",
+        includeNodeLocations: true,
+      }).window;
+
+      var cases = window.sondurumjson[0].gunluk_vaka.split('.').join('');
+      var deaths = window.sondurumjson[0].gunluk_vefat.split('.').join('');
+
+      var yesterdayCases = db.get('yesterday.cases').value();
+      var yesterdayDeaths = db.get('yesterday.deaths').value();
+
+      cases = Number(cases);
+      deaths = Number(deaths);
+
+      var casesGap = cases - yesterdayCases;
+      var deathsGap = deaths - yesterdayDeaths;
+
+      var casesGapStr = (casesGap < 0 ? "" : "+") + casesGap;
+      var deathsGapStr = (deathsGap < 0 ? "" : "+") + deathsGap;
+
       client.post('statuses/update', {
         status: (
-          'COVID-19 in ' + key.country + '\n' +
+          'COVID-19 in Turkey \n'+
           '------------------------' + '\n' +
-          'todayCases: ' + key.todayCases + '\n' +
-          'todayDeaths: ' + key.todayDeaths + '\n' +
+          'yesterdayCases: ' + yesterdayCases + '\n' +
+          'yesterdayDeaths: ' + yesterdayDeaths + '\n' +
+          '------------------------ \n' +
+          'todayCases: ' + cases + '\n' +
+          'todayDeaths: ' + deaths + '\n' +
+          '------------------------ \n' +
+          '' + casesGapStr + ' cases \n' +
+          '' + deathsGapStr + ' deaths \n' +
           '------------------------' + '\n' +
           'posted with Twitter API'
         )
       }).then(result => {
         console.log('You successfully tweeted this: \n"' + result.text + '"');
-      }).catch(console.error);
-    }
-  })
-}).catch((error) => {
-  console.error(error);
+      });
+
+      db.set('yesterday.cases', cases)
+        .write();
+      db.set('yesterday.deaths', deaths)
+        .write();
+    });
+  } catch (e) {}
+}, {
+  timeZone: "Turkey"
 });
